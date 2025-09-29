8e2727c4f5844cf3ef1105bcbc9a41206ae7a233
j-narleski <jaclyn.narleski@duke.edu>
2025-09-29 12:36:41 -0400

    Data dictionary

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -21,6 +21,10 @@ line_list = file_object.readlines()
 #Close the file
 file_object.close()
 
+# Initialize dictionaries
+data_dict = {}
+location_dict = {}
+
 #Pretend we read one line of data from the file
 for lineString in line_list:
     # Check if line is a data line
@@ -36,6 +40,9 @@ for lineString in line_list:
     obs_lc = lineData[4]
     obs_lat = lineData[6]
     obs_lon = lineData[7]
+
+    data_dict[record_id] = obs_date
+    location_dict[record_id] = (obs_lat, obs_lon)
 
     #Print the location of sara
     print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
